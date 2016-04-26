8def3dab8587acb7eeed6185069cea736861f2dc
tranduytung <tranduytung1994@gmail.com>
2016-04-26 20:28:00 +0700

    login and logout function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get "sessions/new"
+
   root "static_pages#home"
 
   get "home" => "static_pages#home"
@@ -9,5 +11,9 @@ Rails.application.routes.draw do
   
   get "signup" => "users#new"
   
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
   resources :users
 end
