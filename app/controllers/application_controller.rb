a4b4953f95a76447999adaa2a95bfd4ee3f72fc1
tranduytung <tranduytung1994@gmail.com>
2016-04-26 21:19:09 +0700

    edit user function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "message.please_log_in"
+      redirect_to login_url
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :find_user, only: [:show, :edit]
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find params[:id]
   end
 
   def create
@@ -18,11 +19,32 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "message.update_profile"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
   
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation, :picture
+  end
+
+  def find_user
+    @user = User.find params[:id]
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user? @user
   end
 end
