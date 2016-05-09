4738b1f4ec7485800cc8062b5a6ca99d5b79d8d7
tranduytung <tranduytung1994@gmail.com>
2016-05-09 16:17:38 +0700

    admin create word with answers

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,7 +1,37 @@
 class Admin::WordsController < ApplicationController
   before_action :require_admin
-
+  before_action :find_category, only: [:new, :create]
+  
   def index
     @words = Word.paginate page: params[:page], per_page: 20
   end
+
+  def new
+    @word = @category.words.build
+  end
+
+  def create
+    @word = @category.words.build category_params
+    if @word .answers.select{|answer| answer.is_correct == true}.size != 1
+      flash[:error] = t "message.should_have_a_correct_answer"
+      render "new"
+    elsif @word.save
+      flash[:success] = t "message.new_word_successful"
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:word).permit :content,
+      answers_attributes: [:content, :is_correct]
+  end
+
+  def find_category
+    @category = Category.find params[:category_id]
+  end
+
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,10 +1,12 @@
 class Word < ActiveRecord::Base
   belongs_to :category
   has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, allow_destroy: true, limit: 4
   
   validates :content, presence: true, length: {maximum: 150}
 
   def correct_answer
     answers.find_by word_id: id ,is_correct: true
   end
+
 end
