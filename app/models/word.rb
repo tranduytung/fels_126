f76ed1cbfdcc50e65b7532058b8ed56414355e14
tranduytung <tranduytung1994@gmail.com>
2016-05-23 16:05:52 +0700

    fix edit words

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ActiveRecord::Base
   belongs_to :category
   has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers, allow_destroy: true
-  before_save :check_correct_answers, :check_answers_count
+  validate :check_correct_answers, :check_answers_count
   scope :not_learned, ->user_id{where "id NOT IN (SELECT word_id FROM results
     WHERE lesson_id IN (SELECT id FROM lessons WHERE user_id = ?))", user_id}
   scope :random, ->{order"RANDOM()"}
@@ -16,20 +16,18 @@ class Word < ActiveRecord::Base
   end
 
   private
-
   def check_correct_answers
-    correct_answer_count = self.answers.select{|answer| answer.is_correct}.size
-    unless correct_answer_count == 1
+    correct_answer_count = self.answers.select{|answer|
+      answer.is_correct? && answer._destroy == false}.size
+    unless correct_answer_count == Settings.answers.correct_answers
       errors.add :base, I18n.t("message.should_have_a_correct_answer")
-      return false
     end
   end
 
   def check_answers_count
     answers_count = self.answers.size
-    unless (answers_count >= 1 && answers_count <= 4)
+    unless (answers_count == Settings.answers.limit_answers)
       errors.add :base, I18n.t("message.check_count_ansewer")
-      return false
     end
   end
 end
