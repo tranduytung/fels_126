0dcbb6aef8c51ef55079d7e65f02fce61e05e506
tranduytung <tranduytung1994@gmail.com>
2016-05-27 08:14:26 +0700

    test 2

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -34,8 +34,8 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def destroy
-    # @category.delete_activity
-    @category.destroy
+    @category.delete_activity
+    # @category.destroy
     flash[:success] = t "message.category_deleted"
     redirect_to admin_categories_path
   end
